083377674557dd1368235a3fd5deeab22d8d55a8
Ibrahim.H <bitsnaps@yahoo.fr>
2020-09-18 20:08:52 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <vue-webrtc ref="webrtc" width="100%" roomId="sample-room"/>
+    Room: sample-room
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import WebRTC from 'vue-webrtc'
 
+import * as io from 'socket.io-client'
+window.io = io
+
+Vue.use(WebRTC)
+// or
+// import {WebRTC} from 'vue-webrtc'
+// Vue.component(WebRTC.name, WebRTC)
 export default {
   name: 'App',
+  mounted: function () {
+    this.$refs.webrtc.join()
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
